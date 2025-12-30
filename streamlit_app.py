c34ad0f17a963649961067745dbf55d4bb06679e
Valentina Idárraga Franco <109562926+vidarragaf@users.noreply.github.com>
2025-12-30 14:46:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-
+import requests
 # Write directly to the app
 st.title(f" :cup_with_straw: Customize Your Smoothie!")
 
@@ -27,13 +27,14 @@ ingredients_list = st.multiselect('Chosse up to 5 ingredients', my_dataframe, ma
 
 
 if ingredients_list:
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
     ingredients_string=''
 
     for fuit_chosen in ingredients_list:
         ingredients_string +=fuit_chosen +' '
-    #st.write(ingredients_string)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+    
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -47,7 +48,8 @@ if ingredients_list:
 
 
 
-import requests
+
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
 sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
